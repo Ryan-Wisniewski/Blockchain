d5b2285f93a70df2a4fd4e8c1e90d693958a0d46
Ryan Wisniewski <51066538+Ryan-Wisniewski@users.noreply.github.com>
2020-01-16 12:29:01 -0500

    Update miner.py

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -2,23 +2,34 @@ import hashlib
 import requests
 
 import sys
+from time import time
 import json
 
 
 def proof_of_work(block):
-    """
-    Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
-    """
-    pass
+        """
+        Simple Proof of Work Algorithm
+        Stringify the block and look for a proof.
+        Loop through possibilities, checking each one against `valid_proof`
+        in an effort to find a number that is a valid proof
+        :return: A valid proof for the provided block
+        """
+        block_string = json.dumps(block, sort_keys=True)
+        proof = 0
+        start_time = time()
+        print('Start time is: ', start_time)
+
+        while valid_proof(block_string, proof) is False:
+            proof += 1
+        end_time = time()
+        print('End time is: ', end_time, '\n')
+        print('Time it took to complete: ', end_time - start_time, '\n')
+        return proof
 
 
 def valid_proof(block_string, proof):
     """
-    Validates the Proof:  Does hash(block_string, proof) contain 6
+    Validates the Proof:  Does hash(block_string, proof) contain 3
     leading zeroes?  Return true if the proof is valid
     :param block_string: <string> The stringified block to use to
     check in combination with `proof`
@@ -27,11 +38,16 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
     # What is the server address? IE `python3 miner.py https://server.com/api/`
+
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
@@ -42,7 +58,7 @@ if __name__ == '__main__':
     id = f.read()
     print("ID is", id)
     f.close()
-
+    coins = 0
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -55,16 +71,26 @@ if __name__ == '__main__':
             print(r)
             break
 
-        # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        # Get the block from `data` and use it to look for a new proof
+        # print(data)
+        new_proof = proof_of_work(data['last_block'])
+        # print(new_proof)
+        
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
-
+        # print(post_data)
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
 
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        
+        if data['message'] == 'Success!':
+            coins += 1
+        else:
+            print(data.message)
+        print('Lambda coins mined: ', coins, '\n')
+        
+        
